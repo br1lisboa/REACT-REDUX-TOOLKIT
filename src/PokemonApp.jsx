cb2c773c6399631d610569f39b1fac459c635de5
br1lisboa <brlisbo@gmail.com>
2022-11-05 13:17:22 -0300

    conectada la store

--- src/PokemonApp.jsx
+++ src/PokemonApp.jsx
@@ -1,9 +1,10 @@
 import { useEffect } from "react"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { getPokemons } from "./store/slices/pokemon/thunks"
 
 export const PokemonApp = () => {
 
+    const { pokemons, page, isLoading } = useSelector(state => state.pokemons)
 
     /* dipatch de mis acciones */
     const dispatch = useDispatch()
@@ -21,11 +22,24 @@ export const PokemonApp = () => {
             <h1>PokemonApp</h1>
             <hr />
 
+            <span>
+                Loading: {isLoading ? 'True' : 'False'}
+            </span>
+
             <ul>
-                <li>Hola</li>
-                <li>Hola</li>
-                <li>Hola</li>
+                {
+                    pokemons.map((pokemon) => (
+                        <li key={pokemon.name}>{pokemon.name}</li>
+                    ))
+                }
             </ul>
+
+            <button
+                disabled={isLoading}
+                onClick={() => dispatch(getPokemons(page))}
+            >
+                Next
+            </button>
 
         </>
 
