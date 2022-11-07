b3a0fef885d0dbbd48e74ea4c4c6fcf50a684d0f
br1lisboa <brlisbo@gmail.com>
2022-11-07 07:13:48 -0300

    probando RTK

---
+++ src/TodoApp.jsx
@@ -0,0 +1,20 @@
+
+
+/* RTK QUERY esta hecho para optimizar las peticiones http */
+
+export const TodoApp = () => {
+    return (
+        <>
+            <h1>Todos - RTK Query</h1>
+            <hr />
+
+            <h4>isLoading...</h4>
+
+            <pre>...</pre>
+
+            <button>
+                Next Todo
+            </button>
+        </>
+    )
+}
