00996ca0af727fe8c72da0c915388c077720d164
br1lisboa <brlisbo@gmail.com>
2022-11-07 07:57:33 -0300

    terminado

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -2,19 +2,55 @@
 
 /* RTK QUERY esta hecho para optimizar las peticiones http */
 
+import { useState } from "react"
+import { useGetTodoQuery, useGetTodosQuery } from "./store/apis/todosApi"
+
 export const TodoApp = () => {
+
+    const [todoId, setTodoId] = useState(1)
+
+    /* const { data: todos = [], isLoading } = useGetTodosQuery() */
+
+    const { data: todo, isLoading } = useGetTodoQuery(todoId)
+
+    const nexTodo = () => {
+        setTodoId(todoId + 1)
+    }
+
+    const prevTodo = () => {
+        if (todoId === 1) return
+        setTodoId(todoId - 1)
+    }
+
     return (
         <>
             <h1>Todos - RTK Query</h1>
             <hr />
 
-            <h4>isLoading...</h4>
+            <h4>isLoading: {isLoading ? 'True' : 'False'}</h4>
 
-            <pre>...</pre>
+            <pre>{JSON.stringify(todo)}</pre>
 
-            <button>
+            <button onClick={prevTodo}>
+                Prev Todo
+            </button>
+
+            <button onClick={nexTodo}>
                 Next Todo
             </button>
+
+            {/* <ul>
+                {todos.map(todo => (
+                    <li key={todo.id}>
+            
+                        <strong>{todo.completed ? 'DONE' : 'Pending'}</strong>
+            
+                        {todo.title}
+                        
+                    </li>
+                ))}
+            </ul> */}
+
         </>
     )
 }
--- src/store/apis/todosApi.js
+++ src/store/apis/todosApi.js
@@ -13,9 +13,14 @@ export const todosApi = createApi({
     endpoints: (builder) => ({
         getTodos: builder.query({
             query: () => '/todos'
-        })
+        }),
+
+        getTodo: builder.query({
+            query: (todoId) => `/todos/${todoId}`
+        }),
+
     })
 
 })
 
-export const { useGetTodosQuery } = todosApi
+export const { useGetTodosQuery, useGetTodoQuery } = todosApi
